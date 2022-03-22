87ed906ebcb60c51709fc83119f1597ab3cfd020
rscres <renatocrescenti@gmail.com>
2022-03-22 18:02:45 +0100

    Created userPlay function

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ const regex = /(rock|paper|scissor)/gi;
 
 function cpuPlay() {
     let index = Math.floor(Math.random() * 3);
-    cpuSelection = choices[index];
+    let cpuSelection = choices[index];
     return cpuSelection;
 }
 
+let userSelection = prompt("Choose rock, paper or scissor: ");
 
+function userPlay() {
+    while (regex.test(userSelection) == false) {
+        userSelection = prompt("Not valid, try again: ");
+        if (userSelection === null) break;
+    } 
+    return userSelection;    
+}
+ 
+console.log(userPlay());
 
 
 
