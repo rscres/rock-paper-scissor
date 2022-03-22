0e82bd8ce93e77b11b5c9ff13ed4d490f968f793
rscres <renatocrescenti@gmail.com>
2022-03-22 20:20:13 +0100

    Added playRound function

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
-const choices = ['Rock', 'Paper', 'Scissor'];
+const choices = ['rock', 'paper', 'scissor'];
 const regex = /(rock|paper|scissor)/gi;
+
 
 function cpuPlay() {
     let index = Math.floor(Math.random() * 3);
-    let cpuSelection = choices[index];
-    return cpuSelection;
+    let cpuChoice = choices[index];
+    console.log(cpuChoice);
+    return cpuChoice;
 }
 
-let userSelection = prompt("Choose rock, paper or scissor: ");
 
 function userPlay() {
-    while (regex.test(userSelection) == false) {
-        userSelection = prompt("Not valid, try again: ");
-        if (userSelection === null) break;
+    let choice = prompt("Choose rock, paper or scissor: ");
+    while (regex.test(choice) == false) {
+        if (choice === null) break;
+        choice = prompt("Not valid, try again: ");   
     } 
-    return userSelection;    
+    console.log(choice);
+    return choice;   
 }
- 
-console.log(userPlay());
+
+let userSelection = userPlay();
+let cpuSelection = cpuPlay();
+
+function playRound(userSelection, cpuSelection) {
+    let result = '';
+    switch(true) {
+        case userSelection == 'paper' && cpuSelection == 'rock':
+            result = "You win!! Paper beats rock";
+            break;
+        case userSelection == 'rock' && cpuSelection == 'scissor':
+            result = "You win!! Rock beats scissor";
+            break;
+        case userSelection == 'scissor' && cpuSelection == 'paper':
+            result = "You win!! Scissor beats paper";
+            break;
+        case cpuSelection == 'paper' && userSelection == 'rock':
+            result = "You lose... paper beats rock";
+            break;
+        case cpuSelection == 'rock' && userSelection == 'scissor':
+            result = "You lose... rock beats scissor";
+            break;
+        case cpuSelection == 'scissor' && userSelection == 'paper':
+            result = "You lose... scissor beats paper";
+            break;
+        default:
+            result = "That's a draw"
+    }
+    return result; 
+}
+
+console.log(playRound(userSelection, cpuSelection))
+
+
 
 
 
